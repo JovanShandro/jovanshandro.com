a1e8282f4a5560baca6dd4d8e40179beb92cc277
Jovan Shandro <shandrojovan@gmail.com>
2020-08-12 11:52:50 +0200

    Add reactstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "reactstrap";
 
 function App() {
   return (
