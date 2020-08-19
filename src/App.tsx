e6041c51161d096dd4a001e745599bd56d222dd9
Jovan Shandro <shandrojovan@gmail.com>
2020-08-19 11:59:43 +0200

    Move navbar to components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./sections/Navbar";
+import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import MainSection from "./sections/Main";
 import AboutSection from "./sections/About";
