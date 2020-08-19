f721d79375c66698d82d3ce791708fcd61f9a920
Jovan Shandro <shandrojovan@gmail.com>
2020-08-19 12:25:53 +0200

    Make contact links functional

--- src/sections/Contacts.tsx
+++ src/sections/Contacts.tsx
@@ -1,23 +1,46 @@
 import React from "react";
 
 const ContactsSection = () => {
+  const openTab = (url: string) => {
+    window.open(url, "_blank");
+  };
+
   return (
     <div className="wrapper contact" id="contact">
       <h1>Contact</h1>
       <div className="icon-wrapper">
-        <div className="circle" onClick={() => "toLinkedIn()"}>
+        <div
+          className="circle"
+          onClick={() =>
+            openTab("https://www.linkedin.com/in/jovan-shandro-8694a7158")
+          }
+        >
           <i className="fab fa-linkedin-in"></i>
         </div>
-        <div className="circle" onClick={() => "toFacebook()"}>
+        <div
+          className="circle"
+          onClick={() => openTab("https://www.facebook.com/shandrojovan")}
+        >
           <i className="fab fa-facebook-f"></i>
         </div>
-        <div className="circle" onClick={() => "toInsta()"}>
+        <div
+          className="circle"
+          onClick={() => openTab("https://www.instagram.com/jovanshandro/")}
+        >
           <i className="fab fa-instagram"></i>
         </div>
-        <div className="circle" onClick={() => "sendmail()"}>
+        <div
+          className="circle"
+          onClick={() => window.open("mailto:shandrojovan@gmail.com")}
+        >
           <i className="far fa-envelope"></i>
         </div>
-        <div className="circle" onClick={() => "toXing()"}>
+        <div
+          className="circle"
+          onClick={() =>
+            openTab("https://www.xing.com/profile/Jovan_Shandro/cv")
+          }
+        >
           <img src="./img/xing.png" alt="" />
         </div>
       </div>
