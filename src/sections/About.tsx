8a9d08e5a14a7a532c276a316cd57d9adba2aae5
Jovan Shandro <jovan.shandro@identinet.io>
2021-06-08 19:23:00 +0200

    Update about and skills sections

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-const AboutSection = () => {
+const AboutSection: React.FC = () => {
   return (
     <div className="wrapper about" id="about">
       <h1>About Me</h1>
       <div className="about-me-info">
         <p>
-          I am a Computer Science student at Jacobs University
-          Bremen. I discovered my passion for coding during high school, which
-          lead me to dive deep into the programming world.
+          I am a Computer Science graduate from Jacobs University Bremen. I
+          discovered my passion for coding during high school, which lead me to
+          dive deep into the programming world.
         </p>
         <p>
           If I would have to choose three words to describe myself, they would
@@ -17,11 +17,16 @@ const AboutSection = () => {
           within a team setting.
         </p>
         <p>
-          Being in the Blockchain Industry for quite some time now, I have
-          realised how powerful this technology can be, and want to discover
-          even more about it. Other than that, of course I cannot forget to
-          mention my love for web and app development using Javascript/Typescript.
+          Having had the opportunity to work on different projects with many
+          technologies in a relatively short amount of time, I have obtained a
+          &nbsp;<em>'You can build anything if you want!'</em> mentality. I have
+          worked on web based projects with different famous frameworks such as
+          React, Vue and Svelte, on decentralized applications using the
+          Ethereum and Tezos Blockchains (the latter regarding NFT minting), and
+          also with Self-Sovereign Identity (SSI).
         </p>
+        <br />
+        <p>Always excited what the future will bring!</p>
         <div className="right-corner">
           <div className="star"></div>
         </div>
