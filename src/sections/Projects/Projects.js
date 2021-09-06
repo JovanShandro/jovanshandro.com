94161c577c6b29934fe2667c05d565fd2f7c20a9
Jovan Shandro <jovan.shandro@identinet.io>
2021-09-06 14:17:52 +0200

    chore: add dropdown instead of buttons in project section

--- src/sections/Projects/Projects.js
+++ src/sections/Projects/Projects.js
@@ -5,19 +5,51 @@ import styles from "./Projects.module.css";
 
 const projectsLabels = Object.keys(projects);
 
+const Dropdown = ({ values, setValue, value }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const selectValue = (val) => {
+    setValue(val);
+    setIsOpen(false);
+  };
+
+  const Icon = isOpen ? (
+    <i className="fas fa-chevron-up"></i>
+  ) : (
+    <i className="fas fa-chevron-down"></i>
+  );
+
+  return (
+    <div className={styles.dropdown}>
+      <div className={styles.text} onClick={() => setIsOpen((v) => !v)}>
+        {value} {Icon} 
+      </div>
+      {isOpen && (
+        <div className={styles.menu}>
+          {values.map((value, index) => (
+            <div key={index}>
+              <div
+                className={styles.dropdownItem}
+                onClick={() => selectValue(value)}
+              >
+                {value}
+              </div>
+              {index !== values.length - 1 && <hr />}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
 const ProjectsSection = () => {
   const [tab, setTab] = useState(projectsLabels[0]);
 
   return (
     <div className={`wrapper ${styles.projects} `} id="projects">
       <h1>Projects</h1>
-      <div className={styles.tabs}>
-        {projectsLabels.map((label) => (
-          <button key={label} onClick={() => setTab(label)}>
-            {label}
-          </button>
-        ))}
-      </div>
+      <Dropdown value={tab} setValue={setTab} values={projectsLabels} />
       <div className="category-projects">
         {projects[tab]
           .slice()
